8578fb8db22d5ff2eb05ecdc350b43a72d304d75
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-03-24 09:58:19 +0100

    routes access

--- App/routing.php
+++ App/routing.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::add('auth', '/api/auth', \Auth::GUEST, '\App\Controller\Auth');
-Route::add('contact', '/api/contact', \Auth::GUEST, '\App\Controller\Contact');
-Route::add('message', '/api/message', \Auth::GUEST, '\App\Controller\Message');
-Route::add('file', '/api/file', \Auth::GUEST, '\App\Controller\File');
+Route::add('contact', '/api/contact', \Auth::USER, '\App\Controller\Contact');
+Route::add('message', '/api/message', \Auth::USER, '\App\Controller\Message');
+Route::add('file', '/api/file', \Auth::USER, '\App\Controller\File');
