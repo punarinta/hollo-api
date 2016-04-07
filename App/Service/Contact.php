0ba09d64cb986325bba65241d4cf306996f44795
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-04-07 13:16:59 +0200

    Contact.sync()

--- App/Service/Contact.php
+++ App/Service/Contact.php
@@ -5,6 +5,17 @@ namespace App\Service;
 class Contact extends Generic
 {
     /**
+     * Returns a contact by its email
+     *
+     * @param $email
+     * @return null|\StdClass
+     */
+    public function findByEmail($email)
+    {
+        return \DB::row('SELECT * FROM contact WHERE email=? AND user_id=? LIMIT 1', [$email, \Auth::user()->id]);
+    }
+
+    /**
      * Returns contacts associated with the current user account
      *
      * @return mixed
@@ -27,11 +38,57 @@ class Contact extends Generic
     }
 
     /**
-     * Calls data sync
+     * Checks if it's necessary to sync a contact and performs this sync
+     *
+     * @param $email
+     * @return array
+     * @throws \Exception
      */
-    public function sync()
+    public function sync($email)
     {
-        $this->conn->getSync(\Auth::user()->ext_id);
+        $syncCount = 0;
+    //    $lastSyncTs = \Auth::user()->last_sync_ts;
+
+        // get remote contact by email
+        if (!$data = $this->conn->getContact(\Auth::user()->ext_id, ['email' => $email]))
+        {
+            throw new \Exception('Error fetching remote contact');
+        }
+
+        $data = $data->getData();
+
+        if ($contact = \Sys::svc('Contact')->findByEmail($email))
+        {
+            // contact exists -> check 'count'
+            if ($data['count'] != $contact->count)
+            {
+                // sync messages
+                $syncCount = $data['count'] - $contact->count;
+            }
+        }
+        else
+        {
+            // contact doesn't exist -> insert
+            \Sys::svc('Contact')->create(array
+            (
+                'user_id'   => \Auth::user()->id,
+                'email'     => $email,
+                'name'      => $data['name'],
+                'count'     => $data['count'],
+            ));
+
+            $syncCount = $data['count'];
+        }
+
+        if ($syncCount)
+        {
+            \Sys::svc('Message')->sync($email, $syncCount);
+        }
+
+        return array
+        (
+            'count' => 0,
+        );
     }
 
     /**
