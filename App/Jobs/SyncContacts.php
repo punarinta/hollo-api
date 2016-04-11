c24d79948493e7d88f1ac185da57d7d20735636e
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-04-11 14:59:38 +0200

    worker sync: id checks

--- App/Jobs/SyncContacts.php
+++ App/Jobs/SyncContacts.php
@@ -14,13 +14,20 @@ class SyncContacts extends Generic
 
     public function perform()
     {
-        if (!$userId = (int) $this->args['user_id'])
+        if (!$user = \Sys::svc('User')->findById($this->args['user_id']))
         {
+            echo "No user found (ID = {$this->args['user_id']}).\n";
             return false;
         }
 
-        \Sys::svc('Contact')->syncAll($userId, true);
+        if (!$user->ext_id)
+        {
+            echo "No account connected. User ID = {$user->id}\n";
+            return false;
+        }
 
-        return false;
+        \Sys::svc('Contact')->syncAll($user->id, true);
+
+        return true;
     }
 }
