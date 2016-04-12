ae27d77aedf48dead27f06008855e35486d69783
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-04-12 17:02:08 +0200

    filter messages by subject

--- App/Controller/Message.php
+++ App/Controller/Message.php
@@ -4,6 +4,14 @@ namespace App\Controller;
 
 class Message extends Generic
 {
+    /**
+     * Returns contacts messages
+     *
+     * @doc-var     (string) subject     - Filter by subject.
+     *
+     * @return mixed
+     * @throws \Exception
+     */
     static public function findByEmail()
     {
         if (!$email = \Input::data('email'))
@@ -11,7 +19,7 @@ class Message extends Generic
             throw new \Exception('Email not provided.');
         }
 
-        return \Sys::svc('Message')->findByContact($email);
+        return \Sys::svc('Message')->findByContact($email, \Input::data('subject'));
     }
 
     static public function reply()
