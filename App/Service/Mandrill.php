36bbf25715c3d6703be864ac44ababe893bbdd84
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-04-12 15:20:49 +0200

    Mandrill service

---
+++ App/Service/Mandrill.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Service;
+
+class Mandrill
+{
+    protected $mandrill = null;
+    
+    public function __construct()
+    {
+        $config = \Sys::cfg(null);
+        $this->mandrill = new \Mandrill($config['mandrill']['api_key']);
+    }
+
+    /**
+     * @param array $to
+     * @param $body
+     * @param string $subject
+     * @return mixed
+     * @throws \Mandrill_Error
+     * @throws \Mandrill_HttpError
+     */
+    public function send($to, $body, $subject = '')
+    {
+        $setup = array
+        (
+            'acync'     => true,
+            'text'      => $body,
+            'subject'   => $subject,
+        );
+
+        foreach ($to as $toAtom)
+        {
+            $setup['to'][] = array
+            (
+                'type'  => 'to',
+                'email' => $toAtom['email'],
+                'name'  => $toAtom['name'],
+            );
+        }
+        
+        return $this->mandrill->call('messages/send-template', $setup);
+    }
+}
