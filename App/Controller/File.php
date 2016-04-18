33b820efeff3f15838adf56951b76dd7a7e4bc9d
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-04-18 12:58:34 +0200

    old code removed

--- App/Controller/File.php
+++ App/Controller/File.php
@@ -31,26 +31,4 @@ class File extends Generic
         
         return \Sys::svc('File')->getProcessorLink($extId);
     }
-
-    /**
-     * Temporary function for demo
-     * 
-     * @throws \Exception
-     */
-    static public function fetch()
-    {
-        if (!$extId = \Input::get('extId'))
-        {
-            throw new \Exception('External ID not provided.');
-        }
-        if (!$type = \Input::get('type'))
-        {
-            throw new \Exception('Type not provided.');
-        }
-
-        header('Content-Type: ' . $type);
-        header('Content-Disposition: inline; filename="filename.pdf"');
-
-        echo \Sys::svc('File')->getProcessorContent($extId);
-    }
 }
