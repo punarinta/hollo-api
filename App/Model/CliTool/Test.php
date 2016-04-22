40463fc74690d68bc9da3bd33a457f9583e610ae
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-04-22 10:51:51 +0200

    oauth tokens are working!

--- App/Model/CliTool/Test.php
+++ App/Model/CliTool/Test.php
@@ -11,8 +11,8 @@ class Test
 {
     public function smtp()
     {
-        \Sys::svc('Smtp')->setupThread(1, 1);
-        \Sys::svc('Smtp')->send([], 'hollo, world!');
+        \Sys::svc('Smtp')->setupThread(1, 1, 'subject');
+        \Sys::svc('Smtp')->send([['email'=>'cheaterx@yandex.ru', 'name'=>'Tet Guy']], 'hollo, world!' . uniqid());
         return "\n";
     }
 
@@ -32,7 +32,7 @@ class Test
         print_r($x * 1);
     }
 
-    public function scope()
+    public function gmail($code = null)
     {
         require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
 
@@ -40,28 +40,43 @@ class Test
         $client->setApplicationName('Hollo app');
         $client->setClientId(\Sys::cfg('social_auth.google.clientId'));
         $client->setClientSecret(\Sys::cfg('social_auth.google.secret'));
-        $client->setRedirectUri('https://api.context.io/connect/oauth2callback');
+        $client->setRedirectUri('https://app.hollo.dev/oauth/google');
         $client->addScope('https://mail.google.com/');
-        $client->setIncludeGrantedScopes(true);
+    //    $client->setIncludeGrantedScopes(true);
         $client->setAccessType('offline');
+        $client->setApprovalPrompt('force');
 
-        //echo $client->createAuthUrl();
+        // Load previously authorized credentials from a file.
+        $credentialsPath = 'google.token';
+        if (file_exists($credentialsPath))
+        {
+            $accessToken = file_get_contents($credentialsPath);
+        }
+        else
+        {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
 
-        $service = new \Google_Service_Gmail($client);
+            // Exchange authorization code for an access token.
+            $accessToken = $client->authenticate($authCode);
 
-        $client->authenticate('4/H4JGXcqD0qAybX1e-rMMK-_nuRUQoIxVbkGolZPtJKU', true);
-        $token = $client->getAccessToken();
-        echo "Token: $token\n";
+            // Store the credentials to disk.
+            file_put_contents($credentialsPath, $accessToken);
+            printf("Credentials saved to %s\n", $credentialsPath);
+        }
+        $client->setAccessToken($accessToken);
 
-    /*    $mail = new \PHPMailer;
-        $mail->SMTPDebug = 3;
+        // Refresh the token if it's expired.
+        if ($client->isAccessTokenExpired())
+        {
+            $client->refreshToken($client->getRefreshToken());
+            file_put_contents($credentialsPath, $client->getAccessToken());
+        }
 
-        $mail->oauthUserEmail = 'hollo.email@gmail.com';
-        $mail->oauthClientId = \Sys::cfg('social_auth.google.clientId');
-        $mail->oauthClientSecret = \Sys::cfg('social_auth.google.secret');
-        $mail->oauthRefreshToken = $token;
-
-        $mail->send();*/
+        echo "Refresh token: " . $client->getRefreshToken();
 
         return "\n";
     }
--- App/Service/Smtp.php
+++ App/Service/Smtp.php
@@ -4,6 +4,9 @@ namespace App\Service;
 use App\Model\ContextIO\ContextIO;
 
 include_once 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+include_once 'vendor/guzzlehttp/promises/src/functions_include.php';
+include_once 'vendor/guzzlehttp/psr7/src/functions_include.php';
+include_once 'vendor/guzzlehttp/guzzle/src/functions_include.php';
 
 class Smtp
 {
@@ -37,12 +40,38 @@ class Smtp
 
         $settings = json_decode($mailbox->settings, true) ?:[];
 
-        $this->mail->Host = $settings['host'];
-        $this->mail->SMTPAuth = (bool) @(strlen($settings['pass']) > 0);
-        $this->mail->Username = $settings['user'];
-        $this->mail->Password = $settings['pass'];
-        $this->mail->SMTPSecure = $settings['sec'];
-        $this->mail->Port = $settings['port'];
+        if ($settings['type'] == 1)
+        {
+            $this->mail->Host = $settings['host'];
+            $this->mail->SMTPAuth = (bool) @(strlen($settings['pass']) > 0);
+            $this->mail->Username = $settings['user'];
+            $this->mail->Password = $settings['pass'];
+            $this->mail->SMTPSecure = $settings['sec'];
+            $this->mail->Port = $settings['port'];
+        }
+        elseif ($settings['type'] == 2)
+        {
+            $token = json_decode(file_get_contents('google.token'), true);
+            $settings['token'] = $token['refresh_token'];
+
+            $this->mail = new \PHPMailerOAuth();
+            $this->mail->isSMTP();
+            $this->mail->Host = 'smtp.gmail.com';
+            $this->mail->SMTPAuth = true;
+            $this->mail->AuthType = 'XOAUTH2';
+            $this->mail->SMTPSecure = 'tls';
+            $this->mail->Port = 587;
+
+            $this->mail->oauthUserEmail = $mailbox->email;
+            $this->mail->oauthClientId = \Sys::cfg('social_auth.google.clientId');
+            $this->mail->oauthClientSecret = \Sys::cfg('social_auth.google.secret');
+            $this->mail->oauthRefreshToken = $settings['token'];
+            $this->mail->SMTPDebug = 4;
+        }
+        else
+        {
+            throw new \Exception('Mailbox type not supported');
+        }
         
         if ($messageId)
         {
@@ -55,41 +84,39 @@ class Smtp
             // get original message data
             $data = $this->conn->getMessage($user->ext_id, ['message_id' => $message->ext_id, 'include_body' => 1]);
 
-            if (!$data)
+            if ($data)
             {
-                return false;
-            }
+                $data = $data->getData();
 
-            $data = $data->getData();
+                $refs = $data['references'];
+                array_push($refs, $data['email_message_id']);
 
-            $refs = $data['references'];
-            array_push($refs, $data['email_message_id']);
+                $this->mail->addCustomHeader('Message-ID: ' . \Text::GUID_v4() . '@' . \Sys::cfg('mailless.this_server'));
+                $this->mail->addCustomHeader('In-Reply-To: ' . $data['email_message_id']);
+                $this->mail->addCustomHeader('References: ' . implode(' ', $refs));
 
-            $this->mail->addCustomHeader('Message-ID: ' . \Text::GUID_v4() . '@' . \Sys::cfg('mailless.this_server'));
-            $this->mail->addCustomHeader('In-Reply-To: ' . $data['email_message_id']);
-            $this->mail->addCustomHeader('References: ' . implode(' ', $refs));
+                $this->mail->Subject = $data['subject'];
 
-            $this->mail->Subject = $data['subject'];
-
-            $body = '';
-            foreach (preg_split("/\r\n|\n|\r/", $data['body'][0]['content']) as $line)
-            {
-                $body .= '> ' . $line;
-            }
-
-            $this->mail->Body = implode("\n", $body);
-
-            if (isset ($data['addresses']['from']))
-            {
-                // reply back to sender
-                $this->mail->addAddress($data['addresses']['from']['email'], $data['addresses']['from']['name']);
-            }
-
-            if (isset ($data['addresses']['cc']))
-            {
-                foreach ($data['addresses']['cc'] as $from)
+                $body = [];
+                foreach (preg_split("/\r\n|\n|\r/", $data['body'][0]['content']) as $line)
                 {
-                    $this->mail->addCC($from['email'], $from['name']);
+                    $body[] = '> ' . $line;
+                }
+
+                $this->mail->Body = implode("\n", $body);
+
+                if (isset ($data['addresses']['from']))
+                {
+                    // reply back to sender
+                    $this->mail->addAddress($data['addresses']['from']['email'], $data['addresses']['from']['name']);
+                }
+
+                if (isset ($data['addresses']['cc']))
+                {
+                    foreach ($data['addresses']['cc'] as $from)
+                    {
+                        $this->mail->addCC($from['email'], $from['name']);
+                    }
                 }
             }
         }
@@ -117,7 +144,7 @@ class Smtp
         {
             throw new \Exception('Send not setup');
         }
-        
+
         $this->mail->Body = $body . "\n" . $this->mail->Body;
 
         if ($to && $subject)
@@ -134,6 +161,8 @@ class Smtp
         {
             throw new \Exception($this->mail->ErrorInfo);
         }
+
+        echo "==============\n";
 
         return true;
     }
