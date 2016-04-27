bb7e9eca288c7ace5900aef2d8a007e04a13dd86
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-04-27 15:48:19 +0200

    sync session on settings update + signature

--- App/Controller/Settings.php
+++ App/Controller/Settings.php
@@ -27,6 +27,7 @@ class Profile extends Generic
      * @doc-var     (int) id!               - Contact ID.
      * @doc-var     (string) firstName      - First name.
      * @doc-var     (string) lastName       - Last name.
+     * @doc-var     (string) signature      - Signature.
      *
      * @throws \Exception
      */
@@ -37,9 +38,11 @@ class Profile extends Generic
 
         if (\Input::data('firstName') !== null) $settings->firstName = \Input::data('firstName');
         if (\Input::data('lastName') !== null) $settings->lastName = \Input::data('lastName');
+        if (\Input::data('signature') !== null) $settings->signature = \Input::data('signature');
 
         // save settings back
         $user->settings = json_encode($settings);
         \Sys::svc('User')->update($user);
+        \Sys::svc('Auth')->sync();
     }
 }
