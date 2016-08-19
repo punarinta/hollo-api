6686613b64d135dce294d27cf0f2774ba316d3f8
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-08-19 15:49:40 +0300

    schema update

--- dev-resources/schema.sql
+++ dev-resources/schema.sql
@@ -3,30 +3,26 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 26, 2016 at 08:04 AM
--- Server version: 5.6.28-0ubuntu0.15.10.1
--- PHP Version: 5.6.11-1ubuntu3.1
+-- Generation Time: Aug 19, 2016 at 03:49 PM
+-- Server version: 5.7.13-0ubuntu0.16.04.2
+-- PHP Version: 7.0.8-0ubuntu0.16.04.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `mailless`
+-- Database: `hollo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contact`
+-- Table structure for table `chat`
 --
 
-CREATE TABLE IF NOT EXISTS `contact` (
+CREATE TABLE IF NOT EXISTS `chat` (
   `id` bigint(20) unsigned NOT NULL,
-  `user_id` bigint(20) unsigned NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `name` varchar(128) DEFAULT NULL,
-  `muted` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `read` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `name` varchar(64) DEFAULT NULL,
   `count` int(10) unsigned NOT NULL DEFAULT '0',
   `last_ts` int(10) unsigned NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -34,12 +30,14 @@ CREATE TABLE IF NOT EXISTS `contact` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contact_message`
+-- Table structure for table `chat_user`
 --
 
-CREATE TABLE IF NOT EXISTS `contact_message` (
-  `contact_id` bigint(20) unsigned NOT NULL,
-  `message_id` bigint(20) unsigned NOT NULL
+CREATE TABLE IF NOT EXISTS `chat_user` (
+  `chat_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `muted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `read` tinyint(1) unsigned NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -54,15 +52,7 @@ CREATE TABLE IF NOT EXISTS `mail_service` (
   `domains` varchar(2048) NOT NULL,
   `cfg_in` varchar(255) DEFAULT NULL,
   `cfg_out` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `mail_service`
---
-
-INSERT INTO `mail_service` (`id`, `name`, `domains`, `cfg_in`, `cfg_out`) VALUES
-  (1, 'Gmail', '|gmail.com|', '{"type":"imap","oauth":true,"host":"imap.gmail.com","port":993}', '{"type":"smtp","oauth":true,"host":"smtp.gmail.com","port":587,"enc":"tls"}'),
-  (2, 'Yandex', '|yandex.ru|yandex.com|ya.ru|', '{"type":"imap","oauth":false,"host":"imap.yandex.com","port":993,"enc":"ssl"}', '{"type":"smtp","oauth":false,"host":"smtp.yandex.com","port":465,"enc":"ssl"}');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -72,12 +62,24 @@ INSERT INTO `mail_service` (`id`, `name`, `domains`, `cfg_in`, `cfg_out`) VALUES
 
 CREATE TABLE IF NOT EXISTS `message` (
   `id` bigint(20) unsigned NOT NULL,
-  `ext_id` char(24) NOT NULL,
-  `from_contact_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `ext_id` varchar(24) NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `chat_id` bigint(20) unsigned NOT NULL,
   `subject` varchar(256) DEFAULT NULL,
   `body` mediumtext CHARACTER SET utf8mb4,
   `files` mediumtext,
   `ts` bigint(20) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `muted`
+--
+
+CREATE TABLE IF NOT EXISTS `muted` (
+  `user` varchar(255) DEFAULT NULL,
+  `domain` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -89,6 +91,7 @@ CREATE TABLE IF NOT EXISTS `message` (
 CREATE TABLE IF NOT EXISTS `user` (
   `id` bigint(20) unsigned NOT NULL,
   `email` varchar(256) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
   `roles` int(10) unsigned NOT NULL DEFAULT '0',
   `ext_id` char(24) DEFAULT NULL,
   `last_sync_ts` int(10) unsigned NOT NULL DEFAULT '1',
@@ -98,75 +101,66 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Table structure for table `muted`
---
-
-CREATE TABLE IF NOT EXISTS `muted` (
-  `user` varchar(255) DEFAULT NULL,
-  `domain` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `contact`
+-- Indexes for table `chat`
 --
-ALTER TABLE `contact`
-ADD PRIMARY KEY (`id`);
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `contact_message`
+-- Indexes for table `chat_user`
 --
-ALTER TABLE `contact_message`
-ADD UNIQUE KEY `link` (`contact_id`,`message_id`) USING BTREE;
+ALTER TABLE `chat_user`
+  ADD UNIQUE KEY `chat_id_user_id` (`chat_id`,`user_id`);
 
 --
 -- Indexes for table `mail_service`
 --
 ALTER TABLE `mail_service`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `message`
 --
 ALTER TABLE `message`
-ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `muted`
 --
 ALTER TABLE `muted`
-ADD UNIQUE KEY `pair` (`user`,`domain`);
+  ADD UNIQUE KEY `pair` (`user`,`domain`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `contact`
+-- AUTO_INCREMENT for table `chat`
 --
-ALTER TABLE `contact`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+ALTER TABLE `chat`
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `mail_service`
 --
 ALTER TABLE `mail_service`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT;
