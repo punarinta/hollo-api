f163d9ec5171456668fc8b3007cdf78ef4ff399d
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-08-24 11:58:41 +0200

    user cleaner

--- App/Model/CliTool/Achtung.php
+++ App/Model/CliTool/Achtung.php
@@ -9,7 +9,7 @@ namespace App\Model\CliTool;
 class Achtung
 {
     /**
-     * Cleans up all the messages, contacts and their links
+     * Cleans up all the messages, chats and their links
      *
      * @return string
      */
@@ -24,40 +24,17 @@ class Achtung
     }
 
     /**
-     * Clear contact/message data for a specified user
+     * Clears data for a specified user
      *
      * @param $userId
      * @return string
      */
     public function clearUser($userId)
     {
-        $messageCount = 0;
-        $contactCount = 0;
+        $this->justRun('DELETE FROM chat_user WHERE user_id=?', [$userId]);
+        $this->justRun('UPDATE `user` SET last_sync_ts=1 WHERE id=?', [$userId]);
 
-        foreach (\Sys::svc('Contact')->findAllByUserId($userId) as $contact)
-        {
-            echo "Clearing contact {$contact->email}\n";
-
-            foreach (\Sys::svc('Message')->findByContactId($contact->id) as $message)
-            {
-                // kill message and its links
-                \Sys::svc('Message')->delete($message);
-                $this->justRun('DELETE FROM contact_message WHERE contact_id=? OR message_id=?', [$contact->id, $message->id]);
-                ++$messageCount;
-            }
-
-            // kill contact
-            \Sys::svc('Contact')->delete($contact);
-            ++$contactCount;
-        }
-
-        echo "Delected $contactCount contacts and $messageCount messages.\n";
-
-        $this->justRun('ALTER TABLE contact AUTO_INCREMENT = 1');
-        $this->justRun('ALTER TABLE message AUTO_INCREMENT = 1');
-        $this->justRun('UPDATE user SET last_sync_ts=1 WHERE id=?', [$userId]);
-
-        return "\n";
+        return "Deleted.\n";
     }
 
     /**
