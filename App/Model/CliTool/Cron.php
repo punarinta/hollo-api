359e375cf061679534e1d67db4174796a457c075
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-09-29 16:46:18 +0200

    comments

--- App/Model/CliTool/Cron.php
+++ App/Model/CliTool/Cron.php
@@ -8,12 +8,19 @@ namespace App\Model\CliTool;
  */
 class Cron
 {
+    /**
+     * @param bool $byTime
+     * @param bool $byCount
+     * @return string
+     */
     public function removeOldMessages($byTime = true, $byCount = false)
     {
         $count = 0;
 
         // This call is too heavy!
         // We must not run this in production
+
+        // TODO: report every 1000 chats
 
         foreach (\Sys::svc('Chat')->findAll() as $chat)
         {
