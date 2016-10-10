5437129f3187c7837c4859101d507c169f6f75d1
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-10-10 10:07:43 +0200

    fetch contacts avatars for a user

--- App/Model/CliTool/Test.php
+++ App/Model/CliTool/Test.php
@@ -107,4 +107,89 @@ class Test
 
         return "$count messages affected\n\n";
     }
+
+    /**
+     * @param int $userId
+     * @return string
+     */
+    public function updateAvatars($userId = 1)
+    {
+        include_once 'vendor/guzzlehttp/psr7/src/functions.php';
+        include_once 'vendor/guzzlehttp/guzzle/src/functions.php';
+        include_once 'vendor/guzzlehttp/promises/src/functions.php';
+
+        $user = \Sys::svc('User')->findById($userId);
+        $settings = json_decode($user->settings, true) ?: [];
+
+        if (!$token = $settings['token'])
+        {
+            return "No refresh token\n";
+        }
+
+        $client = new \Google_Client();
+        $client->setClientId(\Sys::cfg('oauth.google.clientId'));
+        $client->setClientSecret(\Sys::cfg('oauth.google.secret'));
+        $client->refreshToken($token);
+        $accessToken = $client->getAccessToken();
+
+        $pageSize = 25;
+        $pageStart = 1;
+
+        echo "Access token = {$accessToken['access_token']}\n\n";
+
+        while ($pageStart < 5000)   // limit just to be safe
+        {
+            echo "PageStart = $pageStart\n";
+
+            $ch = curl_init("https://www.google.com/m8/feeds/contacts/default/full?start-index=$pageStart&max-results=$pageSize&alt=json&v=3.0&oauth_token=" . $accessToken['access_token']);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $contacts = json_decode(curl_exec($ch), true) ?:[];
+            curl_close($ch);
+
+            foreach ($contacts['feed']['entry'] as $contact)
+            {
+                $image = null;
+
+                if (isset ($contact['link'][0]['href']))
+                {
+                    $url = $contact['link'][0]['href'];
+                    $url = $url . '&access_token=' . urlencode($accessToken['access_token']);
+
+                    $curl = curl_init($url);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                    curl_setopt($curl, CURLOPT_TIMEOUT, 3);
+                    $image = curl_exec($curl);
+                    $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+                    curl_close($curl);
+
+                    if ($httpcode == 200 && isset ($contact['gd$email'][0]['address']))
+                    {
+                        /* $results[] = array
+                        (
+                            'name'  => $contact['title']['$t'],
+                            'email' => $contact['gd$email'][0]['address'],
+                            'image' => base64_encode($image),
+                        ); */
+
+                        $email = $contact['gd$email'][0]['address'];
+
+                        echo "Saving ava for $email...\n";
+                        file_put_contents('data/files/avatars/' . $email, $image);
+                    }
+                }
+            }
+
+            if (count($contacts['feed']['entry']) < $pageSize)
+            {
+                break;
+            }
+            else
+            {
+                $pageStart += $pageSize;
+            }
+        }
+
+        return "\n";
+    }
 }
