1f616ea48238829e745df0444f412e634ae32f61
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-10-12 17:18:09 +0200

    imap inbox experiments

--- App/Model/Inbox/Imap.php
+++ App/Model/Inbox/Imap.php
@@ -8,7 +8,10 @@ namespace App\Model\Inbox;
  */
 class Imap extends Generic implements InboxInterface
 {
+    private $login = null;
     private $password = null;
+    private $in = [];
+    private $imapRef = '';
 
     /**
      * Init and get a password
@@ -29,17 +32,66 @@ class Imap extends Generic implements InboxInterface
         {
             return;
         }
+
+        $key = \Sys::cfg('sys.imap_hash');
+
+        $this->login = $user->email;
+        $this->password = openssl_decrypt($hash, 'aes-256-cbc', $key, 0, $key);
+
+        if (!$this->in = \Sys::svc('MailService')->getCfg($settings['svc']))
+        {
+            return;
+        }
     }
 
     public function checkNew($userId)
     {
+        if (!$box = $this->imap_open())
+        {
+            throw new \Exception('Incorrect username or password.');
+        }
+
+        imap_close($box);
     }
 
     public function getMessages()
     {
+        if (!$box = $this->imap_open())
+        {
+            throw new \Exception('Incorrect username or password.');
+        }
+
+        $MC = imap_check($box);
+
+        $messages = imap_fetch_overview($box, "1:{$MC->Nmsgs}");
+
+        foreach ($messages as $message)
+        {
+            print_r($message);
+            /*$folder = str_replace($this->imapRef, '', imap_base64($folder));
+            echo "Folder = $folder\n";*/
+        }
+
+        imap_close($box);
     }
 
     public function getMessage($messageId)
     {
+    }
+
+    /**
+     * @return resource
+     * @throws \Exception
+     */
+    private function imap_open()
+    {
+        if (!$this->in)
+        {
+            throw new \Exception('Mail service not configured');
+        }
+
+        $this->imapRef = '{' . $this->in['host'] . ':' . $this->in['port'] . '/imap/ssl/novalidate-cert/readonly}';
+
+        return imap_open($this->imapRef, $this->login, $this->password);
     }
 }
