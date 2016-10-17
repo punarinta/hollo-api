5287f0f032fa62f50a71ff244ad0275bd3f64d30
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-10-17 13:15:42 +0200

    mail checker

--- App/Model/CliTool/Cron.php
+++ App/Model/CliTool/Cron.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Model\CliTool;
+use App\Model\Inbox\Inbox;
 
 /**
  * Class Cron
@@ -28,5 +29,45 @@ class Cron
         }
 
         return "Total messaged purged: $count\n";
+    }
+
+    /**
+     * @param int $type     -- TYPE=1 for Google, TYPE=2 for everything else
+     * @return string
+     */
+    public function checkNewMails($type = 1)
+    {
+        $tsAfter = strtotime('yesterday');
+
+        foreach (\Sys::svc('User')->findAllReal() as $user)
+        {
+            microtime(100000);
+
+            $svc = json_decode($user->settings)->svc;
+
+            if (($type == 1 && $svc != 1) || ($type != 1 && $svc == 1))
+            {
+                continue;
+            }
+
+            $inbox = Inbox::init($user);
+
+            if ($inbox->checkNew())
+            {
+                $count = 0;
+                echo "User {$user->id} has new messages. Syncing... ";
+
+                foreach ($inbox->getMessages(['ts_after' => $tsAfter]) as $messageId)
+                {
+                    $count += 1 * !empty(\Sys::svc('Message')->sync($user->id, $messageId, false));
+
+                    usleep(100000);
+                }
+
+                echo "Found $count new.\n";
+            }
+        }
+
+        return '';
     }
 }
