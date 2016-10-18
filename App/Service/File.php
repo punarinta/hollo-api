2551e1af43966432c86f095d414ece7ae4ed1595
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-10-18 09:59:55 +0200

    switch off urls for now

--- App/Service/File.php
+++ App/Service/File.php
@@ -24,17 +24,17 @@ class File extends Generic
                 $user = \Sys::svc('User')->findById($row->ref_id);
                 $userExtId = $user ? $user->ext_id : null;
 
-                if ($url = ($withImageUrl && \Mime::isImage($file['type']) && $userExtId && $count < 10) ? $this->getProcessorLink($userExtId, $file['extId']) : null)
+            /*    if ($url = ($withImageUrl && \Mime::isImage($file['type']) && $userExtId && $count < 10) ? $this->getProcessorLink($userExtId, $file['extId']) : null)
                 {
                     ++$count;
-                }
+                }*/
 
                 $items[] = array
                 (
                     'name'  => $file['name'],
                     'size'  => $file['size'],
                     'type'  => $file['type'],
-                    'url'   => $url,
+                    'url'   => null,
                     'refId' => $row->ref_id,
                     'extId' => @$file['extId'], // temporary files do not have extIds
                 );
@@ -42,33 +42,5 @@ class File extends Generic
         }
 
         return $items;
-    }
-
-    /**
-     * Returns the contents of a file via processor
-     *
-     * @param $userExtId
-     * @param $fileExtId
-     * @return mixed
-     */
-    public function getProcessorContent($userExtId, $fileExtId)
-    {
-        return $this->conn->getFileContent($userExtId, ['file_id' => $fileExtId]);
-    }
-
-    /**
-     * Returns the link to a file via processor
-     *
-     * @param $userExtId
-     * @param $fileExtId
-     * @return mixed
-     */
-    public function getProcessorLink($userExtId, $fileExtId)
-    {
-        return $this->conn->getFileContent($userExtId,
-        [
-            'file_id' => $fileExtId,
-            'as_link' => 1,
-        ]);
     }
 }
