08ea09ff08c36c322453d5fae686b901e1c606e6
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-10-27 16:50:30 +0200

    debugging

--- App/Controller/GmailPush.php
+++ App/Controller/GmailPush.php
@@ -10,13 +10,13 @@ class GmailPush
      */
     static public function index()
     {
-        file_put_contents('data/files/pubsub.log', "Incoming...\n", FILE_APPEND);
-
         $json = json_decode(file_get_contents('php://input'), 1);
+
+        // file_put_contents('data/files/pubsub.log', 'Payload: ' . json_encode($json) . "\n", FILE_APPEND);
 
         if (isset ($json['message']['data']))
         {
-            $x = json_decode(base64_decode($json['message']['data']));
+            $x = json_decode(base64_decode($json['message']['data']), true);
             $user = \Sys::svc('User')->findByEmail($x['emailAddress']);
 
             $messageId = $json['message']['message_id'];
