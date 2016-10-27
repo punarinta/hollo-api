ef03a64f5cf3581e94715f89e3cae2dff99485cb
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-10-27 14:14:55 +0200

    smallfix

--- App/Controller/GmailPush.php
+++ App/Controller/GmailPush.php
@@ -12,7 +12,10 @@ class GmailPush
     {
         $json = json_decode(file_get_contents('php://input'), 1);
 
-        $json['message']['x'] = base64_decode($json['message']['x']['data']);
+        if (isset ($json['message']['data']))
+        {
+            $json['message']['x'] = base64_decode($json['message']['data']);
+        }
 
         file_put_contents('data/files/pubsub.log', json_encode($json), FILE_APPEND);
 
