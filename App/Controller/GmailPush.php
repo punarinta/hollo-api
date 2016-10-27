be5903b1faf3cfb3562d64f035d02b3043608ae5
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-10-27 14:03:53 +0200

    gmail push logger

--- App/Controller/GmailPush.php
+++ App/Controller/GmailPush.php
@@ -10,6 +10,12 @@ class GmailPush
      */
     static public function index()
     {
+        $json = json_decode(file_get_contents('php://input'), 1);
+
+        $json['message']['x'] = base64_decode($json['message']['x']['data']);
+
+        file_put_contents('data/files/pubsub.log', json_encode($json));
+
         return true;
     }
 }
