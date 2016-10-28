aa2027f2a249108a483e7f38a88ec66055968f45
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-10-28 09:07:47 +0200

    fetch gmail message on arrival

--- App/Controller/GmailPush.php
+++ App/Controller/GmailPush.php
@@ -12,18 +12,25 @@ class GmailPush
     {
         $json = json_decode(file_get_contents('php://input'), 1);
 
-        // file_put_contents('data/files/pubsub.log', 'Payload: ' . json_encode($json) . "\n", FILE_APPEND);
+        file_put_contents('data/files/pubsub.log', date('Y-m-d H:i:s') . '|', FILE_APPEND);
 
         if (isset ($json['message']['data']))
         {
             $x = json_decode(base64_decode($json['message']['data']), true);
             $user = \Sys::svc('User')->findByEmail($x['emailAddress']);
 
-            $messageId = $json['message']['message_id'];
-            $json['user_id'] = $user->id;
-        }
+            if (!isset ($json['message']['message_id']))
+            {
+                file_put_contents('data/files/pubsub.log', "NO MSG-ID\n", FILE_APPEND);
+                return false;
+            }
 
-        file_put_contents('data/files/pubsub.log', json_encode($json, JSON_PRETTY_PRINT) . "\n", FILE_APPEND);
+            $messageId = $json['message']['message_id'];
+
+            \Sys::svc('Message')->sync($user->id, $messageId, false, ['noMarks' => true]);
+
+            file_put_contents('data/files/pubsub.log', "{$user->id}|{$messageId}\n", FILE_APPEND);
+        }
 
         return true;
     }
