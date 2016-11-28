cb12e66b09b38485c745f38a4ee23d2af44dfec3
Vladimir Osipov <vladimir.osipov@coursio.com>
2016-11-28 11:31:45 +0100

    reset state

--- App/Model/CliTool/Achtung.php
+++ App/Model/CliTool/Achtung.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Model\CliTool;
+use \MongoDB\Driver\BulkWrite;
 
 /**
  * Class Achtung
@@ -16,61 +17,23 @@ class Achtung
      */
     public function initialState($withUsers = 0)
     {
-        $this->justRun('TRUNCATE table chat');
-        $this->justRun('TRUNCATE table message');
-        $this->justRun('TRUNCATE table chat_user');
+        // truncate chats
+        try
+        {
+            \DB::command(['drop' => 'chat']);
+        }
+        catch (\Exception $e)
+        {
+            echo "Chats were already dropped\n";
+        }
 
         if ($withUsers)
         {
-            // TODO: use 'roles' later
-            $this->justRun('DELETE FROM `user` WHERE settings = ""');
-            $this->justRun('ALTER TABLE `user` AUTO_INCREMENT = 1');
-            $this->justRun('SELECT @i := 0');
-            $this->justRun('UPDATE `user` SET id=(SELECT @i := @i + 1)', []);
+            $bulk = new BulkWrite();
+            $bulk->delete(['roles' => ['$exists' => false]]);
+            $GLOBALS['-DB-L']->executeBulkWrite('hollo.user', $bulk);
         }
 
         return "\n";
-    }
-
-    /**
-     * Clears data for a specified user
-     *
-     * @param $userId
-     * @return string
-     */
-    public function clearUser($userId)
-    {
-        // memorize all the chats the user was member in
-        $chats = \Sys::svc('Chat')->findAllByUserId($userId);
-
-        $this->justRun('DELETE FROM chat_user WHERE user_id=?', [$userId]);
-
-        foreach ($chats as $chat)
-        {
-            if (\Sys::svc('Chat')->countUsers($chat->id) < 2)
-            {
-                // there's only one person left, remove that link too
-                $stmt = \DB::prepare('DELETE FROM chat_user WHERE chat_id=? LIMIT 1', [$chat->id]);
-                $stmt->execute();
-                $stmt->close();
-
-                // kill the chat
-                \Sys::svc('Chat')->delete($chat);
-            }
-        }
-
-        return "Deleted.\n";
-    }
-
-    /**
-     * @param $sql
-     * @param array $params
-     * @throws \Exception
-     */
-    private function justRun($sql, $params = [])
-    {
-        $stmt = \DB::prepare($sql, $params);
-        $stmt->execute();
-        $stmt->close();
     }
 }
