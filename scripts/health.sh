2100f49d85a3604f47c5ce1e958f2cc29f6a0bcc
punarinta <cheaterx@yandex.ru>
2016-12-03 19:10:18 +0100

    plenty of fixes

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-FREE_MEM=$(free | grep Mem | awk '{print $4/$2}')
-FREE_DISK=$(df | grep /dev/sda1  | awk '{print $4/$2}')
-CPU_1_MIN=$(uptime | awk '{print $8}')
+FREE_MEM=$(free | grep Mem | awk '{print $7/$2}')
+FREE_DISK=$(df | grep /$ | awk '{print $4/$2}')
+CPU_5_MIN=$(uptime | awk '{print $11}')
 
-JSON='{"cpuLoad":'${CPU_1_MIN}'"freeMem":'${FREE_MEM}',"freeDisk":'${FREE_DISK}'}'
+JSON='{"cpuLoad":'${CPU_5_MIN}'"freeMem":'${FREE_MEM}',"freeDisk":'${FREE_DISK}'}'
 
 echo $JSON
+exit
 
 curl -i \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
--X POST --data $JSON "http://requestb.in/1lrimbo1"
+-X POST --data $JSON "http://notify.hollo.email:81"
