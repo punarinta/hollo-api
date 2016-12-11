ee143683235515b11a089e2c7169d21fa118f8bb
punarinta <cheaterx@yandex.ru>
2016-12-11 11:48:53 +0100

    updated

--- scripts/health.sh
+++ scripts/health.sh
@@ -2,10 +2,10 @@
 
 FREE_MEM=$(free | grep Mem | awk '{print $7/$2}')
 FREE_DISK=$(df | grep /$ | awk '{print $4/$2}')
-CPU_5_MIN=$(uptime | awk '{print $11}')
+CPU_5_MIN=$(cat /proc/loadavg | awk '{print $2}')
 HOST=$(hostname)
 
-JSON='{"cpuLoad":'${CPU_5_MIN}'"freeMem":'${FREE_MEM}',"freeDisk":'${FREE_DISK}',"hostname":"'${HOST}'"}'
+JSON='{"cpuLoad":'${CPU_5_MIN}',"freeMem":'${FREE_MEM}',"freeDisk":'${FREE_DISK}',"hostname":"'${HOST}'"}'
 
 # echo $JSON
 
